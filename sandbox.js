61b19a86870ff264b82ab60b90f7daedb7882007
enPlace <n.place@protonmail.com>
2021-05-06 17:29:14 -0300

    request in case of error

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,17 @@
 const request = new XMLHttpRequest();
-let url = "https://jsonplaceholder.typicode.com/todos/"
+let url = "https://jsonplaceholder.typicode.com/todoss/"
+let response 
 request.onreadystatechange =()=>{
-    console.log(request, request.readyState)
-}
+   //console.log(request, request.readyState)
+    if(request.readyState===4&&request.status/100 ==2){
+        console.log(request, request.response)
+        response =JSON.parse(request.response)
+    }else if (request.readyState ===4){
+        console.log("can't fetch data. check the url")
 
+    }
+}
 
 
 request.open("GET", url)
 request.send()
-
