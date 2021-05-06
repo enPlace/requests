761794d456b6af5e93864869babe20c85c40ffc3
enPlace <n.place@protonmail.com>
2021-05-06 17:47:48 -0300

    adding errorhandler function to log data or error message

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,31 @@
+function getTodo(callbackFunction){
+
 const request = new XMLHttpRequest();
-let url = "https://jsonplaceholder.typicode.com/todoss/"
+let url = "https://jsonplaceholder.typicode.com/todos/"
 let response 
+
+
 request.onreadystatechange =()=>{
-   //console.log(request, request.readyState)
+   console.log(request, request.readyState)
     if(request.readyState===4&&request.status/100 ==2){
-        console.log(request, request.response)
-        response =JSON.parse(request.response)
+        callbackFunction(undefined, request.responseText)
+        response =JSON.parse(request.responseText)
     }else if (request.readyState ===4){
-        console.log("can't fetch data. check the url")
+        callbackFunction("Couldn't fetch data. Try double-checking the url.")
 
     }
 }
-
-
 request.open("GET", url)
 request.send()
+}
+
+function errorHandler(error, data){
+    console.log("callbackFunction executed")
+    if(data){
+        console.log(data)
+    }
+    if(error){
+        console.log(error)
+    }
+}
+getTodo(errorHandler)
