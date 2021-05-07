53db8911f4c656b26cca79aa55fd5e6208284cda
enPlace <n.place@protonmail.com>
2021-05-07 13:09:52 -0300

    working with JSON files

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,17 @@
+let response 
+let url = "todos.json"
+
+//xmlhttp request function. 
 const getTodo=(callbackFunction)=>{
 
-const request = new XMLHttpRequest();
-let url = "https://jsonplaceholder.typicode.com/todos/"
-let response 
-
+    const request = new XMLHttpRequest();
+    console.log(request.readyState)
 
 request.onreadystatechange =()=>{
    console.log(request, request.readyState)
     if(request.readyState===4&&request.status/100 ==2){
         callbackFunction(undefined, request.responseText)
-        response =JSON.parse(request.responseText)
+        response = JSON.parse(request.responseText)
     }else if (request.readyState ===4){
         callbackFunction("Couldn't fetch data. Try double-checking the url.")
 
@@ -18,13 +20,15 @@ request.onreadystatechange =()=>{
 request.open("GET", url)
 request.send()
 }
-console.log(1)
+
+
+console.log("hello1")
 console.log(2)
 
 function errorHandler(error, data){
     console.log("callbackFunction executed")
     if(data){
-        console.log(data)
+        console.log("request complete. type 'response' in the console.")
     }
     if(error){
         console.log(error)
